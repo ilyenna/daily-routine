301ae0866873d8e4911b13513833b48b0fbb9d29
ilyenna <ilyenna@protonmail.com>
2021-05-27 22:23:34 -0400

    More figuring out the jQuery. Moving on to the CSS Grid next

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
 $(document).ready(function() {
 
-  // Mouse Enter '#hello'
-  $('#hello').mouseenter(function() {
+  // Mouse Enter '#animals'
+  $('#animals').mouseenter(function() {
     $(this).css("background-color", "#0000ff");
-    $(this).css("width", "50em");
+    $(this).css("width", "50%");
+    $(this).css("font-family", "who asks satan");
+    $(this).css("color", "black");
+    $(this).css("font-size", "20px");
   });
 
-  // Mouse Leave '#hello'
-  $('#hello').mouseleave(function() {
+  // Mouse Leave '#animals'
+  $('#animals').mouseleave(function() {
     $(this).css("background-color", "#fff");
     $(this).css("width", "5em");
+    $(this).css("color", "white");
+    $(this).css("font-size", "15px");
+  });
 
+  // Mouse Enter '#bohemians'
+  $('#bohemians').mouseenter(function() {
+    $(this).css("background-color", "#00ff00");
+    $(this).css("width", "50%");
+    $(this).css("font-family", "who asks satan");
+    $(this).css("color", "black");
+    $(this).css("font-size", "20px");
+  });
+
+  // Mouse Leave '#bohemians'
+  $('#bohemians').mouseleave(function() {
+    $(this).css("background-color", "#fff");
+    $(this).css("width", "5em");
+    $(this).css("color", "white");
+    $(this).css("font-size", "15px");
+  });
+
+    // Mouse Enter '#coven'
+  $('#coven').mouseenter(function() {
+    $(this).css("background-color", "#ff0000");
+    $(this).css("width", "50%");
+    $(this).css("font-family", "who asks satan");
+    $(this).css("color", "black");
+    $(this).css("font-size", "20px");
+  });
+  
+    // Mouse Leave '#coven'
+  $('#coven').mouseleave(function() {
+    $(this).css("background-color", "#fff");
+    $(this).css("width", "5em");
+    $(this).css("color", "white");
+    $(this).css("font-size", "15px");
   });
 });
