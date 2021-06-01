8613c4a29285c38ede7fe9e7cf51e378e8671452
ilyenna <ilyenna@protonmail.com>
2021-05-31 21:28:31 -0400

    Still figuring stuff out don't mind me

--- main.js
+++ main.js
@@ -1,90 +1,28 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  // let animals = document.getElementById("animals");
-  // let bohemians = document.getElementById("bohemians");
-  // let coven = document.getElementById("coven");
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   let animals = document.getElementById("animals");
+//   let bohemians = document.getElementById("bohemians");
+//   let coven = document.getElementById("coven");
 
-  // if (animals === null) {
-  //   console.log("Null");
-  // } else { console.log("Not null"); }
+//   if (animals === null) {
+//     console.log("Null");
+//   } else { console.log("Not null"); }
 
-  // function animalsAnimate () {
-  //   animals.style.backgroundColor = "#ff0000";
-  //   animals.style.width = "100%";
-  // }
+//   function animalsAnimate () {
+//     animals.style.backgroundColor = "#ff0000";
+//     animals.style.width = "100%";
+//   }
 
-  // function bohemiansAnimate() {
-  //   bohemians.style.backgroundColor = "#00ff00";
-  //   bohemians.style.width = "100%";
-  // }
+//   function bohemiansAnimate() {
+//     bohemians.style.backgroundColor = "#00ff00";
+//     bohemians.style.width = "100%";
+//   }
 
-  // function covenAnimate() {
-  //   coven.style.backgroundColor = "#0000ff";
-  //   coven.style.width = "100%";
-  // }
+//   function covenAnimate() {
+//     coven.style.backgroundColor = "#0000ff";
+//     coven.style.width = "100%";
+//   }
 
-  // animals.onclick = animalsAnimate;
-  // bohemians.onclick = bohemiansAnimate;
-  // coven.onclick = covenAnimate;
-});
-
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-
-//   // Mouse Enter '#animals'
-//   $('#animals').mouseenter(function() {
-//     $(this).css("background-color", "#0000ff");
-//     $(this).css("width", "50%");
-//     $(this).css("font-family", "who asks satan");
-//     $(this).css("color", "black");
-//     $(this).css("font-size", "20px");
-//   });
-
-//   // Mouse Leave '#animals'
-//   $('#animals').mouseleave(function() {
-//     $(this).css("background-color", "#fff");
-//     $(this).css("width", "5em");
-//     $(this).css("color", "white");
-//     $(this).css("font-size", "15px");
-//   });
-
-//   // Mouse Enter '#bohemians'
-//   $('#bohemians').mouseenter(function() {
-//     $(this).css("background-color", "#00ff00");
-//     $(this).css("width", "50%");
-//     $(this).css("font-family", "who asks satan");
-//     $(this).css("color", "black");
-//     $(this).css("font-size", "20px");
-//   });
-
-//   // Mouse Leave '#bohemians'
-//   $('#bohemians').mouseleave(function() {
-//     $(this).css("background-color", "#fff");
-//     $(this).css("width", "5em");
-//     $(this).css("color", "white");
-//     $(this).css("font-size", "15px");
-//   });
-
-//     // Mouse Enter '#coven'
-//   $('#coven').mouseenter(function() {
-//     $(this).css("background-color", "#ff0000");
-//     $(this).css("width", "50%");
-//     $(this).css("font-family", "who asks satan");
-//     $(this).css("color", "black");
-//     $(this).css("font-size", "20px");
-//   });
-  
-//     // Mouse Leave '#coven'
-//   $('#coven').mouseleave(function() {
-//     $(this).css("background-color", "#fff");
-//     $(this).css("width", "5em");
-//     $(this).css("color", "white");
-//     $(this).css("font-size", "15px");
-//   });
+//   animals.onclick = animalsAnimate;
+//   bohemians.onclick = bohemiansAnimate;
+//   coven.onclick = covenAnimate;
 // });
